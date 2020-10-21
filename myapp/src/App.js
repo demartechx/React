67abd2f2842ccb0663191b083efdec32199eec27
Demartechx <adeagbothompson@gmail.com>
2020-10-21 15:48:32 +0100

    First react APP

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
+import  './Ninjas.css';
+
+
 
 class App extends Component {
   state = {
@@ -9,12 +13,40 @@ class App extends Component {
       { name: 'Thompson', age: 20, belt: 'blue', id: 3}
     ]
   }
+  addNinja = (ninja) => {
+    //console.log(ninja);
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  componentDidMount(){
+    console.log('component mounted');
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    console.log('component updated');
+    console.log(prevProps, prevState);
+  }
+
   render(){
     return (
       <div className="App">
       <h1>My first React App!</h1>
       <p>Welcome :)</p>
-      <Ninjas ninjas={this.state.ninjas}/>
+      <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja}/>
+      <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
